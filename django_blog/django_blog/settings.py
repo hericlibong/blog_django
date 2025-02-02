96719b4b3730456e38409414ce7f81b3abadfd11
héric Libong <hericlibong@gmail.com>
2025-02-02 02:44:10 +0100

    correction : database connexion with render2

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -96,25 +96,36 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 #     }
 # }
 
-if os.getenv('GITHUB_ACTIONS') == 'true' or os.getenv('DOCKER_ENV') == 'true':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('POSTGRES_DB'),
-            'USER': config('POSTGRES_USER'),
-            'PASSWORD': config('POSTGRES_PASSWORD'),
-            'HOST': config('POSTGRES_HOST', default='db'),  # "db" = utilisé en local
-            'PORT': config('POSTGRES_PORT', default=5432, cast=int),
-        }
+# if os.getenv('GITHUB_ACTIONS') == 'true' or os.getenv('DOCKER_ENV') == 'true':
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': config('POSTGRES_DB'),
+#             'USER': config('POSTGRES_USER'),
+#             'PASSWORD': config('POSTGRES_PASSWORD'),
+#             'HOST': config('POSTGRES_HOST', default='db'),  # "db" = utilisé en local
+#             'PORT': config('POSTGRES_PORT', default=5432, cast=int),
+#         }
+#     }
+# elif os.getenv('RENDER') == 'true':  
+#     DATABASES = {
+#         'default': dj_database_url.config(default=config('DATABASE_URL'))
+#     }
+# else:
+#     DATABASES = {
+#         'default': dj_database_url.config(default=config('DATABASE_URL'))
+#     }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST'),
+        'PORT': config('POSTGRES_PORT', default=5432, cast=int),
     }
-elif os.getenv('RENDER') == 'true':  
-    DATABASES = {
-        'default': dj_database_url.config(default=config('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(default=config('DATABASE_URL'))
-    }
+}
 
 
 
