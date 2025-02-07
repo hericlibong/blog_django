3319c5c824b45d916addc5533e615e119a815fde
héric Libong <hericlibong@gmail.com>
2025-02-07 17:50:26 +0100

    exp create superuser3

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)  # Ajouté pour le déploiement sur Render
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
 
