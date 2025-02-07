abd56ab25ddd2e698039a3f83ba4f1e9c6bba2ab
héric Libong <hericlibong@gmail.com>
2025-02-07 14:10:58 +0100

    suppress create superuser

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -126,17 +126,6 @@ else:
         'default': dj_database_url.config(default=config('DATABASE_URL'))
     }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('POSTGRES_DB'),
-#         'USER': config('POSTGRES_USER'),
-#         'PASSWORD': config('POSTGRES_PASSWORD'),
-#         'HOST': config('POSTGRES_HOST'),
-#         'PORT': config('POSTGRES_PORT', default=5432, cast=int),
-#     }
-# }
-
 
 
 # Password validation
