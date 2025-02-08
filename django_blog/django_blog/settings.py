d16f06da75861034fdf99407edf2f2352f340bc2
héric Libong <hericlibong@gmail.com>
2025-02-08 16:17:22 +0100

    correction: bad cloudinary jobs correction4

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -171,9 +171,9 @@ USE_TZ = True
 # Configuration spécifique pour Cloudinary
 
 cloudinary.config(
-    cloud_name=config('CLOUDINARY_CLOUD_NAME', default=''),
-    api_key=config('CLOUDINARY_API_KEY', default=''),
-    api_secret=config('CLOUDINARY_API_SECRET', default='')
+    cloud_name=config('CLOUDINARY_CLOUD_NAME'),
+    api_key=config('CLOUDINARY_API_KEY'),
+    api_secret=config('CLOUDINARY_API_SECRET')
 )
 
 # Static files (CSS, JavaScript, Images)
